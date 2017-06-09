9d0bd2f6ede4f29906e8e04d1e7cb6695a4a8e3b
promethwill88 <wfong.bk@gmail.com>
2017-06-08 21:43:56 -0700

    sessions controller create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,16 @@ class SessionsController < ApplicationController
 		@user = User.new
 	end
 
+	def create
+		user_params = params.require(:user).permit(:email, :password)
+		# this confirms email/pw is correct
+		@user = User.confirm(user_params)
+		if @user
+			login(@user)
+			redirect_to @user
+		else
+			redirect_to login_path
+		end
+	end
+
 end
