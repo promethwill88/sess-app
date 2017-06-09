f460fe66d9929fafa1a49d86a7800924adeaf588
promethwill88 <wfong.bk@gmail.com>
2017-06-08 22:23:45 -0700

    added security so only current logged in users can view profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
 
 	def show
 	   	@user = User.find_by_id(params[:id])
+	   	if current_user != @user
+	   		redirect_to root_path, notice: "Access Denied"
+	   	end
 	end
 
 	# PRIVATE
