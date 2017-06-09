ce70187794dcdbba747cd074abe8af252baa855d
promethwill88 <wfong.bk@gmail.com>
2017-06-09 14:05:20 -0700

    final commit for controllers

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -17,11 +17,27 @@ class TracksController < ApplicationController
 		@track = Track.find_by_id(params[:id])
 	end
 
+	# def edit
+	# 	@track = set_track
+	# end
+
+	# def update
+		# respond_to do |f|
+		# 	if @track.update{track_params_update}
+		# 		f.json { render :show, status: :ok, location: @track}
+		# 	else
+		# 		f.json { render json:}
+	# end
+
 	# PRIVATE
 	private
 
 	def track_params
 		params.require(:track).permit(:artist, :title, :url, :notes)
 	end
+
+	# def track_params_update
+ #      params.require(:track).permit(:artist, :title, :url, :notes, tracks_attributes: [:id,  :_destroy])
+ #    end
 
 end
