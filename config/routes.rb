e28fa8926b07634fdbb78fcd3896e7f68093e10a
promethwill88 <wfong.bk@gmail.com>
2017-06-14 12:30:10 -0700

    date formatting

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 	get '/events/:id', to: 'events#show', as: 'event'
 	# get '/events/:id/edit', to: 'events#edit', as: 'edit_event'
 	# patch '/events/:id', to: 'events#update'
-	# delete '/events/:id', to: 'events#destroy'
+	delete '/events/:id', to: 'events#destroy'
 
 	# Attendance
 	post '/events/:event_id/attendances', to: 'attendances#create', as: 'attendances'
